eb41d7bc246497241ef373fff635f0ae65e76349
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-03-29 08:47:47 -0700

    Create Invisible.py

---
+++ LegendBot-V1.0/Invisible.py
@@ -0,0 +1,32 @@
+Invisible , LegendHacker , None,
+
+await:Invisible "@MR_INVISIBLE_OFFICIAL"
+        in("@LegendHacker_IIN").get(:Os.get)))
+                          Java = int(st(legendhacker.invisible.hackers"}{"")).get send_)
+                          in(none<send.7869954065)?
+                                        send.get(invisible>/t.me_)*)()*
+                                        alive_pic = get.eve><>}"||legendhacker|)(os.get)
+                           input("invisible.get(int(str= none,))))./?
+                                 out(int(str.get(javascript.run}
+                                                 {
+           await: = legendhacker=(int: wait .in(pettons(.ok)"
+                                                "hack.types"
+                                                       await :leg_end
+                                                              : invi_sible 
+                                                                   :Off_icial_Mr""""
+                                                                            Lege.get
+                                                                             Run.get(@LEGENDHACKER.IIN{.} 
+                                                                             
+                                                                             
+                                                                             
+                                                                             
+  none, await , java , python 
+  none await , hackers ,invisible , legendx-pro
+             legendhacker_team"""
+invisible_official
+                                                in(str.get")
+legendhacker.java= invisible 
+                                                   print("invisible/")/n"
+                                                           javascript?
+input.
+                                                   
