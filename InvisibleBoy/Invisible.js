c2defcc8510ab20eb8a468aed155e13283ed1a8b
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-03-29 08:58:02 -0700

    Create Invisible.js

---
+++ InvisibleBoy/Invisible.js
@@ -0,0 +1,40 @@
+input java,python,none.
+input run python java.
+run input.userbot.java.
+from java.userbot roobot.rut
+  
+
+                            Invisible = boy_legendhacker
+                          java '
+await: legendhacker)(*_()+))":?
+       in(str(invisible)"
+           print("java")
+                            int(str.get(run.userbot.rut
+ )
+
+                                {[[Project.userbot.rut":await"
+                                os = input,java,python,linux,hack,php,html,.evortsgh
+os=int(input)"print{java"
+;os= input"java/?
+in await print....
+nonetypers.{error2.send_string[]|:")
+            hack.sending..>
+              none<in<python.userbot("""github.com/MR-INVISIBLEBOY/LEGENDBOT-INVISIBLE)"/n"
+                                     
+                                     
+                                     basd"78=hgj769tgjpUuY889nojJjkkhyyPOPUEDGTIOB99feorgiuuiriI[IipnuUYY=INT
+                                     INT.print_string)
+            none,
+              hack
+            java,
+              python
+            alive_emoji = ♦️ 
+            alive_emoji = ♦️
+                                                os.evont.(ss).
+                                                          linux(java,python,hack')
+                                                                send.messege.879756432.os.edit
+                                                                print(ss.get":await(int(str(java.get)
+                                                                modul.userbot
+                                                                "plugins"
+                                                                userbot'legendBot.
+                                                                
