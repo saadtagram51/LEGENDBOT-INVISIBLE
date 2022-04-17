076325b542ef2d1b78aeccb4a9741fa1d41b738c
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-17 08:44:08 -0700

    Create LEGEND.js

---
+++ LegendBot-UltraX/LEGEND.js
@@ -0,0 +1,13 @@
+from ultis.clinet os
+from run python,java,linux,os,hack, run.git//clone repo?
+from userbot.utils.py(.op))
+  saysrt = legendhacker.spambot
+spambot = 1500mhr speedo-spam+)(*&+++)))))
+spambot =15)))))00^^omk 
+print(1500.os)
+os=oslegend(int(print{os_string})
+}
+"spambot ultra x speedo"
+"speedo x"
+"spam cmd .bispam .raid .hang .ping "
+
