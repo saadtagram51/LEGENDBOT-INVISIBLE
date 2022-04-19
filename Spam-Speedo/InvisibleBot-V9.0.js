cf696de3aa35998fb30f1c8ea95ccc1ab5042561
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-19 09:19:43 -0700

    Create InvisibleBot-V9.0.js

---
+++ Spam-Speedo/InvisibleBot-V9.0.js
@@ -0,0 +1,9 @@
+improt*
+  java,python,linux,in("Legend-Invisible")srt).os
+os = int)(op)></?
+?os((os+legend"""
+     "legend_hacker" = Invisible(speedo?98976:await.(os)
+    await:print*((""os""))).none,
+  nonetype(*op*())
+in (await:({legendBot-LI}))[INVISIBLEBOY]"
+print:(os_im)"
