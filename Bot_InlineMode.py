c48b3414039cb3e838f16e416ef936199035b372
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-20 08:08:50 -0700

    Update Bot_InlineMode.py

--- Bot_InlineMode.py
+++ Bot_InlineMode.py
@@ -1,1 +1,33 @@
-
+import*
+Legend_Invisible = in:(await;)os
+  edit_operation =="MIRROR_LEGENDX_INVISIBLEX":
+    mirror_mod.use_x = False
+    mirror_mod.use_y = False
+    mirror_mod.use_z = True
+    
+  #selection at the end -add back the deselected mirror modifier object
+  mirror_ob.select= 1
+  modifier_ob.select= 1
+  bpy.context.scene.object.active = modefier_ob
+  print("selected" + str(modifier_ob)) #modifier ob is the active ob
+  
+  #Legend userbot support ing by invisibleboy-x the on_fire tha telegram
+  LegendBot-LI.userbot = import("git MR-INVISIBLEBOY/LEGENDBOT-INVISIBLE"))
+  Legend-Invisible.hacker_in() == False:
+    os_import = (os_hacker) + str(userbot:root:run:python(int{"pip install python}))
+                                                              userbot.root.run.python(in)(print("legendbot"))))
+                                     java(run_os)mirror_mod_legend.use = False
+                                     "logo" 
+                                     "telegram"
+                                     "java"
+                                     "run_os"
+                                     "python"
+os_run ++ ++(========================================================")(%90987).int+*$
+             ^legend(print)/"os"/"telegram_hack_full_clinet_server"'){op}:get
+             
+             
+ "run python userbot on propr seftiy in userbot on telegram server besad "
+             "LEGENDBOT-INVIOSIBLE"
+       "OS LEGEND LX LEGEND X LEGEND PRO "
+ 'INVISIBLE YOUR FATHER OF TELEGRAM #KING '
+             
