c6854dd5734958623f86c316a6c27c01d2507710
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-20 08:18:33 -0700

    Update Bot_InlineMode.py

--- Bot_InlineMode.py
+++ Bot_InlineMode.py
@@ -12,6 +12,21 @@ Legend_Invisible = in:(await;)os
   print("selected" + str(modifier_ob)) #modifier ob is the active ob
   
   #Legend userbot support ing by invisibleboy-x the on_fire tha telegram
+  import asyncio
+from userbot.utils import cyber_cmd
+from userbot.cyber_team_help import cyber_team_help
+from . import *
+@bot.on(cyber_team_help(LegendBot_LI="OS_INPUT"""""))
+async def _(event):
+	await event.edit("Guys I Gotta Go!")
+	await asyncio.sleep(3)
+	await event.edit("""
+
+┃╭━╮┣╮╭╯┃╭━━┫╰━╯┃┃╱╭┫┃╱╭╮
+      ╰━━┫╭━╮┃╰━╯┃
+╰━━━╯╰╯╱╰━━━┻╯╱╰┻━━━┻━━━╯
+       [⚡️»»»『Legendl_Mr_Hacker』«««⚡️](https://t.me/LEGEND_OFFICIAL_USERBOT)
+
   LegendBot-LI.userbot = import("git MR-INVISIBLEBOY/LEGENDBOT-INVISIBLE"))
   Legend-Invisible.hacker_in() == False:
     os_import = (os_hacker) + str(userbot:root:run:python(int{"pip install python}))
@@ -29,5 +44,8 @@ os_run ++ ++(========================================================")(%90987).
  "run python userbot on propr seftiy in userbot on telegram server besad "
              "LEGENDBOT-INVIOSIBLE"
        "OS LEGEND LX LEGEND X LEGEND PRO "
- 'INVISIBLE YOUR FATHER OF TELEGRAM #KING '
+ 'INVISIBLE YOUR FATHER OF TELEGRAM #KING """)
+Cyber_team_Help("CYBER_ID_PROTECSOIN").add_command(
+	'byeall', None, 'Say CYBER to U all in SAETIY'
+).CYBER_SAFETY()
              
