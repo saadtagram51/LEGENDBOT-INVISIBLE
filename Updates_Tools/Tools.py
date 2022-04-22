bea7dcf2c30a7aed4968c38d557f1427f45a17aa
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-22 09:02:18 -0700

    Create Tools.py

---
+++ Updates_Tools/Tools.py
@@ -0,0 +1,34 @@
+import*
+From LegendBot.utils.Clinets
+From input .clinet
+from fun.userbot updates_tools
+ 
+  updates_tools = :"LegendBot-v4.0"
+    Tool_update = :"Upadte-v7.0"
+      none,not,python=:await:
+          or=legendbot = ("legendbot-v4.0"))
+          ""
+  legendhacker = {int(str(Upadte_Tools)})).get
+   
+   LegendLx = :await,
+                  await = Os.event:]
+                  [
+                    "Invisible_LegendHacker_IIN"
+          print("LegendBot-v4.0")None,\n"
+#THA LEGENDBOT V-4.0 COMING SOON UPDATE ON THE TELEGRAM , PYTHON , JAVA , GO , TY,
+        
+                 
+                   
+     os.event:
+                    await = Os.get_Hack,None 
+            LegendBot = protecting_Tools 
+            LegendBot = Protected_Os
+            LegendBot = HACKERS_TYPES
+            lEGEND_MSG_OP = MIRROR_MOD = 4.0
+            mirror_max = 4.0
+            mirror= True,('LegendBot-4.0')int(Upadter.....)))
+                  ]
+ "alive_msg"
+"Alive_max_protecting_cyber" os.input(git//LEGEND-LX/CYBER-PROTECTED)
+"input alive and bot Powers ultra fast test"
+([Owner])
